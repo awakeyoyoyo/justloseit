1e27f8cebaf25fd7ad47f9c2b7494f42596be405
awakeyoyoyo <lqh19980716@163.com>
2023-08-03 21:13:58 +0800

    net模块-编写协议管理模块

---
+++ net/src/main/java/com/awake/net/protocol/ProtocolManager.java
@@ -0,0 +1,12 @@
+package com.awake.net.protocol;
+
+/**
+ * @version : 1.0
+ * @ClassName: ProtocolManager
+ * @Description: 协议管理
+ * @Auther: awake
+ * @Date: 2023/8/3 21:10
+ **/
+public class ProtocolManager implements IProtocolManager {
+
+}
