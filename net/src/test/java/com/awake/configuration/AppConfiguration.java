81d69cd38550eaac40fea919ceaabca33cc85d51
awakeyoyoyo <lqh19980716@163.com>
2023-10-16 10:48:26 +0800

    net-测试用例完善

--- net/src/test/java/com/awake/configuration/AppConfiguration.java
+++ net/src/test/java/com/awake/configuration/AppConfiguration.java
@@ -25,7 +25,6 @@ import org.springframework.context.annotation.Configuration;
  **/
 
 @Configuration
-//@ComponentScans(value = {@ComponentScan("com.awake.server")})
 public class AppConfiguration {
 
     @Configuration
--- net/src/test/java/com/awake/server/ApplicationConfiguration.java
+++ net/src/test/java/com/awake/server/ApplicationConfiguration.java
@@ -2,14 +2,17 @@ package com.awake.server;
 
 import com.awake.NetContext;
 import com.awake.net.config.ConfigManager;
+import com.awake.net.config.model.ConsumerProperties;
 import com.awake.net.config.model.NetConfig;
+import com.awake.net.config.model.ProviderProperties;
+import com.awake.net.config.model.RegistryProperties;
 import com.awake.net.protocol.ProtocolManager;
 import com.awake.net.protocol.properties.ProtocolProperties;
 import com.awake.net.router.PacketBus;
 import com.awake.net.router.Router;
 import com.awake.net.session.SessionManager;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
@@ -28,43 +31,59 @@ public class ApplicationConfiguration {
 
 
     @Configuration
+    @EnableConfigurationProperties({RegistryProperties.class
+            , ProviderProperties.class
+            , ConsumerProperties.class
+            , ProtocolProperties.class})
     public static class NetAutoConfiguration {
-        @Bean
-        @ConditionalOnBean(NetConfig.class)
-        public ConfigManager configManager(NetConfig netConfig) {
-            var configManager = new ConfigManager();
-            configManager.setLocalConfig(netConfig);
-            return configManager;
-        }
-        //引入模块
+
+        //配置-整合
         @Bean
         @ConditionalOnMissingBean
-        public ProtocolManager protocolManager(ProtocolProperties protocolProperties) {
-            return new ProtocolManager(protocolProperties);
+        public NetConfig netConfig() {
+            return new NetConfig();
         }
 
+        //配置管理
+        @Bean
+        @ConditionalOnMissingBean()
+        public ConfigManager configManager() {
+            return new ConfigManager();
+        }
+
+        //协议解析
         @Bean
         @ConditionalOnMissingBean
-        public NetContext netContext() {
-            return new NetContext();
+        public ProtocolManager protocolManager() {
+            return new ProtocolManager();
         }
 
+        //路由
         @Bean
         @ConditionalOnMissingBean
         public Router router() {
             return new Router();
         }
-//
+
+        //协议管理
         @Bean
         @ConditionalOnMissingBean
-        public PacketBus packetService() {
+        public PacketBus packetBus() {
             return new PacketBus();
         }
 
+        //会话状态管理
         @Bean
         @ConditionalOnMissingBean
         public SessionManager sessionManager() {
             return new SessionManager();
+        }
+
+        //上下文管理
+        @Bean
+        @ConditionalOnMissingBean
+        public NetContext netContext() {
+            return new NetContext();
         }
     }
 
