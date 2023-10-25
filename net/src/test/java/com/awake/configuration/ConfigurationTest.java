d406decd1507d2f31bdf46f198e564359ba55751
awakeyoyoyo <lqh19980716@163.com>
2023-10-25 18:07:49 +0800

    net- 测试registerVo转换

--- net/src/test/java/com/awake/configuration/ConfigurationTest.java
+++ net/src/test/java/com/awake/configuration/ConfigurationTest.java
@@ -1,8 +1,12 @@
 package com.awake.configuration;
 
 import com.awake.net.config.ConfigManager;
+import com.awake.net.config.model.ConsumerProperties;
 import com.awake.net.config.model.NetConfig;
+import com.awake.net.config.model.ProviderProperties;
+import com.awake.net.consumer.registry.RegisterVO;
 import com.awake.net.protocol.ProtocolManager;
+import io.netty.util.NetUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +33,37 @@ public class ConfigurationTest {
         System.out.println(netConfig);
         System.out.println(protocolManager);
         System.out.println(configManager);
+    }
+
+    @Test
+    public void registerVoTest() {
+        // 服务提供者模块列表和服务提供者配置
+        // 定义2个服务提供者模块
+        ProviderProperties provider = netConfig.getProvider();
+        ConsumerProperties consumer = netConfig.getConsumer();
+        var vo =  RegisterVO.valueOf(provider, consumer);
+        var voStr = vo.toString();
+
+        // test | 127.0.0.1:80 | provider:[100-aaa-a, 120-bbb-b] | consumer:[100-aaa-random-a, 120-bbb-random-b]
+        System.out.println(voStr);
+
+        var newVo = RegisterVO.parseString(voStr);
+        System.out.println(newVo.toString());
+////        Assert.assertEquals(vo, newVo);
+
+        // /127.0.0.1
+        System.out.println(NetUtil.LOCALHOST);
+
+        // localhost/127.0.0.1
+        System.out.println(NetUtil.LOCALHOST4);
+
+        //localhost/0:0:0:0:0:0:0:1
+        System.out.println(NetUtil.LOCALHOST6);
+
+        // 200
+        System.out.println(NetUtil.SOMAXCONN);
+
+        // name:lo (Software Loopback Interface 1)
+        System.out.println(NetUtil.LOOPBACK_IF);
     }
 }
