941ce1af80c32d391e0885a97c5d41f27f6d1d52
awakeyoyoyo <lqh19980716@163.com>
2023-11-30 17:31:38 +0800

    storage-初始化工程

---
+++ storage/src/main/java/com/awake/storage/StorageContext.java
@@ -0,0 +1,11 @@
+package com.awake.storage;
+
+/**
+ * @version : 1.0
+ * @ClassName: StorageContext
+ * @Description: TODO
+ * @Auther: awake
+ * @Date: 2023/11/30 17:31
+ **/
+public class StorageContext {
+}
