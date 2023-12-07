d88d03c0874800e09da65fe10a0d67a233cf1dc4
awakeyoyoyo <lqh19980716@163.com>
2023-12-07 16:22:10 +0800

    net-消息队列

---
+++ net/src/main/java/com/awake/net/router/SignalBridge.java
@@ -0,0 +1,72 @@
+package com.awake.net.router;
+
+import com.awake.net.router.attachment.SignalAttachment;
+import com.awake.util.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+/**
+ * @version : 1.0
+ * @ClassName: SignalBridge
+ * @Description: 同步或异步的调用控制器，同步和异步调用的信号沟通桥梁
+ * @Auther: awake
+ * @Date: 2023/12/7 16:19
+ **/
+public class SignalBridge {
+    private static final Logger logger = LoggerFactory.getLogger(SignalBridge.class);
+
+    // equal with 32767
+    private static final int SIGNAL_MASK = 0B00000000_00000000_01111111_11111111;
+
+    /**
+     * key：signalId
+     */
+    private static final Map<Integer, SignalAttachment> signalAttachmentMap = new ConcurrentHashMap<>(1000);
+    private static final AtomicReferenceArray<SignalAttachment> signalAttachmentArray = new AtomicReferenceArray<>(SIGNAL_MASK + 1);
+
+    public static void addSignalAttachment(SignalAttachment signalAttachment) {
+        var signalId = signalAttachment.getSignalId();
+        var hash = signalId & SIGNAL_MASK;
+
+        // Using an Atomic Reference Array is just to improve performance, and only using a ConcurrentHashMap will still work
+        if (signalAttachmentArray.compareAndSet(hash, null, signalAttachment)) {
+            return;
+        }
+
+        signalAttachmentMap.put(signalId, signalAttachment);
+    }
+
+    public static SignalAttachment removeSignalAttachment(SignalAttachment signalAttachment) {
+        return removeSignalAttachment(signalAttachment.getSignalId());
+    }
+
+    public static SignalAttachment removeSignalAttachment(int signalId) {
+        var hash = signalId & SIGNAL_MASK;
+
+        var attachment = signalAttachmentArray.get(hash);
+        if (attachment != null && attachment.getSignalId() == signalId) {
+            // 使用性能更高的lazySet()，当我们想对一个原子变量进行修改，而且我们知道这个修改不需要立即对其他线程可见。
+            signalAttachmentArray.lazySet(hash, null);
+            return attachment;
+        }
+        return signalAttachmentMap.remove(signalId);
+    }
+
+    public static void status() {
+        var count = 0;
+        for (int i = 0; i < SIGNAL_MASK + 1; i++) {
+            var value = signalAttachmentArray.get(i);
+            if (value != null) {
+                logger.info("signalPacketArray has attachment [index:{}][count:{}][value:{}]", i, ++count, JsonUtils.object2String(value));
+            }
+        }
+
+        signalAttachmentMap.forEach((key, value) -> {
+            logger.info("signalAttachmentMap has attachment [key:{}][value:{}]", key, JsonUtils.object2String(value));
+        });
+    }
+}
