212d6b1969b1c2e519d3a98145d70a06b7a9d379
awake <lqh19980716@163.com>
2024-04-01 16:09:54 +0800

    hello- init

---
+++ hello/src/main/java/org/hello/gamemodule/role/RoleController.java
@@ -0,0 +1,11 @@
+package org.hello.gamemodule.role;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author：lqh
+ * @Date：2024/4/1 16:08
+ */
+@Controller
+public class RoleController {
+}
---
+++ hello/src/main/java/org/hello/gamemodule/role/RoleService.java
@@ -0,0 +1,11 @@
+package org.hello.gamemodule.role;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author：lqh
+ * @Date：2024/4/1 16:09
+ */
+@Service
+public class RoleService {
+}
