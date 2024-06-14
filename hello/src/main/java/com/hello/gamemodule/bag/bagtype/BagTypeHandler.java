286a65c8ee552d20943a6ce83e8972c6405830e6
awake <lqh19980716@163.com>
2024-06-14 15:58:05 +0800

    hello-背包模块

--- hello/src/main/java/com/hello/gamemodule/bag/bagtype/BagTypeHandler.java
+++ hello/src/main/java/com/hello/gamemodule/bag/bagtype/BagTypeHandler.java
@@ -6,12 +6,27 @@ package com.hello.gamemodule.bag.bagtype;
  */
 public interface BagTypeHandler {
 
+    /**
+     * 增加物品
+     */
     void addItem();
 
+    /**
+     * 删除物品
+     * @return
+     */
     boolean removeItem();
 
+    /**
+     * 检测是否满足
+     * @return
+     */
     boolean checkEnough();
 
+    /**
+     * 获取物品数量
+     * @return
+     */
     long getItemNum();
 
 }
